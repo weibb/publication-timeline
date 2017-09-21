0fce0435e7481cb6059edce140ae7d418ff10fab
equinoxfires <evan@brinkbit.com>
2017-09-21 16:27:21 -0400

    Divide time events into publication year

--- src/Timeline.js
+++ src/Timeline.js
@@ -7,29 +7,24 @@ export class Timeline extends Component {
         this.state = props.state;
         this.getSummary = props.getSummary;
     }
-    render() {
-        if ( this.props.matches && this.props.matches.length > 0) {
+    render(){
+        if( this.props.matches && this.props.matches.length > 0) {
             const pubs = this.props.matches;
-            return (
-                <div>
-                {pubs.map( event =>
-                (<TimeEvent id={event.id} getSummary={this.getSummary} />)
-            )}
-        </div>)
+            const timeline = [];
+            for( let year = 1970; year <= 2017; year += 1){
+                const pubsByYear = [];
+                pubs.map( event =>
+                    {
+                        if (event.date == year){
+                            pubsByYear.push(<TimeEvent id={event.id} className='timeEvent' getSummary={this.getSummary} />)
+                        }
+                    }
+                );
+                let jsx = <div id={year} className='year'>{pubsByYear}</div>;
+                console.log(pubsByYear);
+                timeline.push(jsx);
+            }
+            return (<div id='timeline-inner'>{timeline}</div>);
         }
-        else return( <div className="row">
-            <div id="years" className="row">
-                <div className="year" id="d0">0</div>
-                <div className="year" id="d10">1</div>
-                <div className="year" id="d20">2</div>
-                <div className="year" id="d30">3</div>
-                <div className="year" id="d40">4</div>
-                <div className="year" id="d50">5</div>
-                <div className="year" id="d60">6</div>
-                <div className="year" id="d70">7</div>
-                <div className="year" id="d80">8</div>
-                <div className="year" id="d90">9</div>
-            </div>
-        </div> );
     }
-};
+}
