3fbaf85822d591f77a4f58901ef9fe933fc9035c
equinoxfires <evan@brinkbit.com>
2017-10-12 14:44:01 -0400

    Populate author info from config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,13 @@
 import { pubs } from './data';
 console.log(pubs);
 export const config = {
+    author: {
+        creditName: 'Gehlbach PL',
+        displayName: 'Peter L. Gehlbach',
+        degrees: 'MD, PhD',
+        institution: 'Johns Hopkins University',
+        photo: "/media/2.png",
+    },
     pubs: pubs,
     categories: [
         'all',
